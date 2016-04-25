ddd54f04aba01e7c9038112cb2a3860308cad647
Oleh-Hrebchuk <oleh.hrebchuk@gmail.com>
2016-04-25 12:19:59 +0300

    graphs commit

---
+++ rate_counter/rate/tasks.py
@@ -0,0 +1,140 @@
+# -*- encoding: utf-8 -*-
+
+from rate import *
+from celery.task import periodic_task
+#from .models import Institute, StudentGroup, Subject
+import requests
+import logging
+from bs4 import BeautifulSoup
+import re
+import os
+import subprocess
+
+
+from django.core.wsgi import get_wsgi_application
+os.environ['DJANGO_SETTINGS_MODULE'] = 'rate_counter.settings'
+application = get_wsgi_application()
+from models import TableRow
+
+@periodic_task(run_every=60*60)  # periodicity of execution in seconds (1 hour)
+def parse_data():
+
+    get_institutes()
+    get_logger().info('Completed institutes collection')
+    get_groups()
+    get_logger().info('Completed groups collection')
+    get_subjects()
+    get_logger().info('Completed subjects collection')
+    get_logger().info('DB is up-to-date')
+
+
+def get_institutes():
+
+    soup = set_bs(get_url())
+
+    # in source code of NULP site's schedule page all names and IDs of institutes and groups are located in <option> tag
+    # list of all of the institutes is present on any schedule page,
+    # list of groups is present in page, URL of which contain ID of related institute
+
+    # Filling database with Institute objects
+    for option in soup.find_all('option'):
+        if len(option.text) == 4:
+            inst_obj, created = Institute.objects.get_or_create(name=option.text, site_id=option.get('value'))
+            message(inst_obj, created)
+
+
+def get_groups():
+
+    for inst_obj in Institute.objects.all():
+        get_logger().info(u"Processing Institute {}".format(inst_obj.name))
+        soup = set_bs(get_url(str(inst_obj.site_id)))
+
+        for option in soup.find_all('option'):
+            if re.match(ur'.+-.+', option.text):
+                grp_obj, created = StudentGroup.objects.get_or_create(name=option.text,
+                                                                      site_id=option.get('value'),
+                                                                      institute=inst_obj)
+                message(grp_obj, created)
+
+
+def get_subjects():
+    sem = 1
+    sem_part = 1
+    for grp_obj in StudentGroup.objects.all():
+        get_logger().info(u"Processing Group {0} of {1} Institute".format(grp_obj.name, grp_obj.institute.name))
+        soup = set_bs(get_url(grp_obj.institute.site_id, grp_obj.site_id, str(sem), str(sem_part)))
+
+        # all information about subject (name, lecturer) located in div object with such class
+        subjects = soup.find_all('div', {'class': 'vidst'})
+
+        for subject in subjects:
+            # getting first component of div object (directly the name)
+            sub_obj, created = Subject.objects.get_or_create(name=subject.contents[0].text, group=grp_obj)
+            message(sub_obj, created)
+
+
+def get_logger():
+    logger = logging.getLogger(__name__)
+    logger.setLevel(logging.INFO)
+    return logger
+
+
+def message(obj, created):
+    """
+    Creating a message if new object is added
+    """
+    if created:
+        get_logger().info(u"Adding {}".format(obj.name))
+
+
+def set_bs(url):
+    """
+    setting up a BeautifulSoup
+    """
+    r = requests.get(url)
+    return BeautifulSoup(r.content, "html.parser")
+
+
+def get_url(inst='1', group='', semestr='1', semestr_part='1'):
+    """
+    Forming URL of page for parsing data
+    """
+    return 'http://www.lp.edu.ua/node/40?inst={0}&group={1}&semestr={2}&semest_part={3}'.format(inst,
+                                                                                                group,
+                                                                                                semestr,
+                                                                                                semestr_part,
+                                                                                                )
+
+
+def rate_graphs(id):
+    '''
+    prepare data for json column charts
+    '''
+    get_data = TableRow.objects.filter(table_id=id)
+    list = []
+    tmp=[]
+    tmp_name=[]
+    for line in get_data:
+        tmp_name.append(line.name)
+        tmp.append(line.point)
+    list.append(tmp_name)
+    list.append(tmp)
+    x = 'Rate Graphs'
+    y = '100'
+    list[0][0]=x
+    list[1][0]=y
+
+    return list
+
+def rate_graphs_pie(id):
+    '''
+    prepare data for json pie charts
+    '''
+    get_data = TableRow.objects.filter(table_id=id)
+    list=[['Rate','Subject']]
+    for line in get_data:
+        tmp=[]
+        tmp.append(line.name)
+        tmp.append(line.point)
+        list.append(tmp)
+    return list
