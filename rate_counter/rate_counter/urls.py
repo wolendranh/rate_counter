125a1623b92d6dd5cf0db57c1015f0e817609689
Scadnie <grizly.vl@gmail.com>
2016-05-11 21:36:06 +0300

    add login application
    *start new app accounts
    *add login, logout and register views
    *add login and register forms
    *add crispy_forms to project

--- rate_counter/rate_counter/urls.py
+++ rate_counter/rate_counter/urls.py
@@ -17,10 +17,14 @@ from django.conf.urls import url, include
 from django.contrib import admin
 from django.conf import settings
 from django.conf.urls.static import static
-
+from accounts.views import login_view, logout_view, register_view
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^rate/', include('rate.urls', namespace='rate'))
+    url(r'^rate/', include('rate.urls', namespace='rate')),
+    url(r'^login/', login_view, name='login'),
+    url(r'^logout/', logout_view, name='logout'),
+    url(r'^register/', register_view, name='register'),
+
 
 ]
 
